e595811ecb6dcdd7bb304156d524e126cafdcbf1
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-05 06:17:38 +0100

    Update keys_inventory_items.sql

--- keys_inventory_items.sql
+++ keys_inventory_items.sql
@@ -1,13 +1,10 @@
 CREATE TABLE IF NOT EXISTS `keys_inventory_items` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `inventory_name` VARCHAR(100) NOT NULL,    -- Nom de l'inventaire ('vehicle_keys' ou 'property_keys')
-    `name` VARCHAR(100) NOT NULL,               -- Nom de la clé (ex : 'car_key_ABC123')
-    `plate` VARCHAR(20) NOT NULL,               -- Plaque du véhicule (si applicable)
-    `count` INT NOT NULL,                       -- Nombre d'exemplaires (généralement 1)
-    `owner` VARCHAR(60) DEFAULT NULL,           -- ID du joueur propriétaire de la clé
-    `shared_with` TEXT DEFAULT NULL,            -- Liste des joueurs avec qui la clé est partagée (stockée sous forme de chaîne séparée par des virgules)
+    `inventory_name` VARCHAR(100) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `count` INT NOT NULL,
+    `owner` VARCHAR(60) NOT NULL,
     PRIMARY KEY (`id`),
-    INDEX `index_keys_inventory_items_inventory_name_name_plate` (`inventory_name`, `name`, `plate`),
-    INDEX `index_keys_inventory_items_inventory_name_name_owner` (`inventory_name`, `name`, `owner`),
-    INDEX `index_keys_inventory_inventory_name` (`inventory_name`)
+    INDEX `index_keys_inventory_items_inventory_name_name` (`inventory_name`, `name`),
+    INDEX `index_keys_inventory_items_inventory_name_name_owner` (`inventory_name`, `name`, `owner`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
