47c74b970a1e2fe74f027b2f06a32f03c5d0285e
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-05 06:17:03 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+Config.VehicleKeyPrefix = 'vehicle_key_' -- Préfixe pour les clés de véhicules
+Config.PropertyKeyPrefix = 'property_key_' -- Préfixe pour les clés de propriétés
+
+-- Définir si vous souhaitez activer les notifications de verrouillage/déverrouillage
+Config.EnableLockNotifications = true
