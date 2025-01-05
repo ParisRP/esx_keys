1293eea4847f553216e2b42b9760d2a5f29b32ac
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-05 05:58:11 +0100

    Update user_property_keys.sql

--- user_property_keys.sql
+++ user_property_keys.sql
@@ -1,7 +1,13 @@
-CREATE TABLE IF NOT EXISTS `user_property_keys` (
+CREATE TABLE IF NOT EXISTS `keys_inventory_items` (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `player_id` INT NOT NULL,
-    `property_id` INT NOT NULL,
+    `inventory_name` VARCHAR(100) NOT NULL,    -- Nom de l'inventaire ('vehicle_keys' ou 'property_keys')
+    `name` VARCHAR(100) NOT NULL,               -- Nom de la clé (ex : 'car_key_ABC123')
+    `plate` VARCHAR(20) NOT NULL,               -- Plaque du véhicule (si applicable)
+    `count` INT NOT NULL,                       -- Nombre d'exemplaires (généralement 1)
+    `owner` VARCHAR(60) DEFAULT NULL,           -- ID du joueur propriétaire de la clé
+    `shared_with` TEXT DEFAULT NULL,            -- Liste des joueurs avec qui la clé est partagée (stockée sous forme de chaîne séparée par des virgules)
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`player_id`) REFERENCES `users`(`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    INDEX `index_keys_inventory_items_inventory_name_name_plate` (`inventory_name`, `name`, `plate`),
+    INDEX `index_keys_inventory_items_inventory_name_name_owner` (`inventory_name`, `name`, `owner`),
+    INDEX `index_keys_inventory_inventory_name` (`inventory_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
