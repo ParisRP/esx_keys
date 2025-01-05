d2c856119bb354846a2aaebd62a6379e474bcb83
Paris RP <40249959+ParisRP@users.noreply.github.com>
2025-01-05 06:14:24 +0100

    Delete locales directory

--- locales/en.lua
+++
@@ -1,7 +0,0 @@
-Locales['en'] = {
-    ['used_vehicle_key'] = 'You have used your vehicle key.',
-    ['used_property_key'] = 'You have used your property key.',
-    ['not_your_vehicle'] = 'This is not your vehicle.',
-    ['key_received'] = 'You have received a key.',
-    ['already_have_key'] = 'You already have a key.',
-}
